436a8b4b9812c9ec4d7d80ca7bceced14ba747b7
Peter Varshavsky <pvarsh@gmail.com>
2014-09-27 18:14:31 -0400

    Multiple plots work, but not xlim, ylim

--- assignment_3/hw3.py
+++ assignment_3/hw3.py
@@ -7,6 +7,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+from math import ceil
+
 import csv
 
 
@@ -39,7 +41,6 @@ cols1b = ['rns', 'mrt', 'smsa', 'med', 'iq',
 
 def scPlots(dfIn):
   # makes scatter plots of all columns against last columns
-  print 'aaa'  
   # get column names for all but last column
   cols1c = list(dfIn.columns.values)[0:-1]
   
@@ -73,9 +74,9 @@ def scPlots(dfIn):
     plt.axis([xmin, xmax, ymin, ymax])
     plt.show()
 
-def scPlotsFig(dfIn):
+def scPlotsFig(dfIn, ncols = 3):
   # makes scatter plots of all columns against last columns on one figure
-  print 'aaa'  
+  
   # get column names for all but last column
   cols1c = list(dfIn.columns.values)[0:-1]
   
@@ -87,28 +88,28 @@ def scPlotsFig(dfIn):
   ywidth = ymax - ymin
   ymin, ymax = ymin - 0.05 * ywidth, ymax + 0.05 * ywidth
   
-  #cols1c = ['rns', 'mrt', 'smsa', 'kww', 'expr']
-  
-  #df1c = df[cols1c].describe()
-  #ymin, ymax = df[cols1b].describe()['lw'][[3,-1]]
-  #ywidth = ymax - ymin
-  #ymin, ymax = ymin - 0.05 * ywidth, ymax + 0.05 * ywidth
-  
   fig = plt.figure()
   
   nplots = len(cols1c)
+  nrows = int(ceil(float(nplots/ncols)))
 
-  for col in cols1c:
-    plt.plot(dfIn[col], dfIn['lw'], 'co')
-    plt.xlabel(col)
-    plt.ylabel('log wage')
-    xmin, xmax = dfSummary[col][[3, -1]]
-    xwidth = xmax - xmin
-    xmin, xmax = xmin - 0.05 * xwidth, xmax + 0.05 * xwidth
+  for i, col in enumerate(cols1c):
+    subPlotPosition = int(str(nrows) + str(ncols) + str(i+1))
+    #print subPlotPosition 
+    ax = fig.add_subplot(subPlotPosition)
+    ax.plot(dfIn[col], dfIn['lw'], 'co')
+    ax.plt.xlabel(col)
+    #ax.ylabel('log wage')
+    #xmin, xmax = dfSummary[col][[3, -1]]
+    #xwidth = xmax - xmin
+    #xmin, xmax = xmin - 0.05 * xwidth, xmax + 0.05 * xwidth
       
-    plt.axis([xmin, xmax, ymin, ymax])
-    plt.show()
-scPlots(df[cols1b])
+    #ax.plt.axis([xmin, xmax, ymin, ymax])
+  plt.show()
+
+scPlotsFig(df[cols1b])
+
+#scPlots(df[cols1b])
 #fout = "table1b.csv"
 #with open(fout, 'r') as f:
 #  writer = csv.writer(f)
