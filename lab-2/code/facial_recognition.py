da54ef3b71d411024bafcb186ef412061e89ca5f
Peter Varshavsky <pvarsh@gmail.com>
2014-10-20 04:12:35 -0400

    Running but bad

--- lab-2/code/facial_recognition.py
+++ lab-2/code/facial_recognition.py
@@ -1,6 +1,8 @@
 from pybrain.datasets.supervised import SupervisedDataSet 
 from pybrain.tools.shortcuts import buildNetwork
 from pybrain.supervised.trainers import BackpropTrainer
+from pybrain.structure import SigmoidLayer
+
 import cv2
 import glob
 import os
@@ -28,16 +30,20 @@ def print_f_list(flist, name = ""):
 if __name__ == "__main__":
     path_pos = "/Users/petervarshavsky/Documents/Git_NYU/applied_data_science/lab-2/images/faces/my-face/resized/"
     path_neg = "/Users/petervarshavsky/Documents/Git_NYU/applied_data_science/lab-2/images/other_faces_resized/"
-    
+   
+    # truth:positive
+    true_val = 1
+    false_val = -1
+ 
     # making training set of positive images
-    files_pos = [(f, 2) for f in glob.glob(path_pos + "*.png")]
+    files_pos = [(f, true_val) for f in glob.glob(path_pos + "*.png")]
     random.shuffle(files_pos)
     splt = int(len(files_pos) * 2.0/3)
     files_pos_train = files_pos[:splt]
     files_pos_test = files_pos[splt:]
 
     # making training set of negative images
-    files_neg = [(f, 1) for f in glob.glob(path_neg + "*.png")]
+    files_neg = [(f, false_val) for f in glob.glob(path_neg + "*.png")]
     random.shuffle(files_neg)
     splt = int(len(files_neg) * 2.0/3)
     files_neg_train = files_neg[:splt]
@@ -58,21 +64,22 @@ if __name__ == "__main__":
     t = loadImage(f)
     
     # build network
-    net = buildNetwork(len(t), int(.03*len(t)), 1)
+    net = buildNetwork(len(t), int(.05*len(t)), int(0.05*len(t)), 1)
    
     # initialize data set
     ds = SupervisedDataSet(len(t), 1)
     ds.addSample(t, truth)
     # add the rest of elements to data set
     for img, truth in train:
+        print truth
         ds.addSample(loadImage(img), truth)
     
 
     # train the network
     trainer = BackpropTrainer(net, ds)
-    error = 10
+    error = 1 # was 10
     iteration = 0
-    while error > 0.0001: 
+    while error > 0.0001: # was 0.0001 
         error = trainer.train()
         iteration += 1
         print "Iteration: {0} Error {1}".format(iteration, error)
